d71aab95d9b910b2147c29c69cf327e805a26266
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2021-11-07 15:58:00 +0100

    test(database): add login test

---
+++ Assets/Project/EditModeTests/UserQueries.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Project.Database;
+using Project.Security;
+using Zenject;
+
+namespace Project.EditModeTests
+{
+    public sealed class UserQueries : ZenjectUnitTestFixture
+    {
+        [Inject] private Database.UserQueries queries;
+        
+        [SetUp]
+        public void Install()
+        {
+            HasherInstaller.InstallFromResource(Container);
+            DatabaseInstaller.InstallFromResource(Container);
+            Container.Inject(this);
+        }
+        
+        [TestCase("ValidUsername", "ValidPassword", ExpectedResult = false)]
+        public bool Try_Login(string username, string password) => queries.TryLogin(username, password, out _);
+    }
+}
