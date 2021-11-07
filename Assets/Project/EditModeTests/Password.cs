2fdff20e20c61e8f29efd2580e22f5d091c3e03c
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2021-11-07 05:00:22 +0100

    test(login): test username and password validation

---
+++ Assets/Project/EditModeTests/Password.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using static Project.Login.Password;
+
+namespace Project.EditModeTests
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public sealed class Password
+    {
+        [TestCase("", ExpectedResult = ValidationResult.InvalidLength)]
+        [TestCase("aaaabbb", ExpectedResult = ValidationResult.InvalidLength)]
+        [TestCase("aaaabbbb", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("aaaa bbbb", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("aaaa愛bbbb", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("[aaaabbbb]", ExpectedResult = ValidationResult.Valid)]
+        public ValidationResult Validate(string password) => Login.Password.Validate(password);
+    }
+}
---
+++ Assets/Project/EditModeTests/Username.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+using static Project.Login.Username;
+
+namespace Project.EditModeTests
+{
+    [SuppressMessage("ReSharper", "StringLiteralTypo")]
+    public sealed class Username
+    {
+        [TestCase("", ExpectedResult = ValidationResult.InvalidLength)]
+        [TestCase("a", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("aaaabbbbccccddddeeeeffffgggghhhh", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("aaaabbbbccccddddeeeeffffgggghhhhi", ExpectedResult = ValidationResult.InvalidLength)]
+        [TestCase("aaaa bbbb", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("aaaa愛bbbb", ExpectedResult = ValidationResult.Valid)]
+        [TestCase("[aaaabbbb]", ExpectedResult = ValidationResult.Valid)]
+        public ValidationResult Validate(string username) => Login.Username.Validate(username);
+    }
+}
