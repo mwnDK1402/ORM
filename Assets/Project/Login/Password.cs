bb57d485afca858cae856b6a1a7ed1a54422055d
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2021-11-07 04:59:44 +0100

    feat(login): add username and password validation

---
+++ Assets/Project/Login/Password.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project.Login
+{
+    public static class Password
+    {
+        public const int MinLength = 8;
+        public const int MaxLength = 128;
+
+        public static ValidationResult Validate(string password)
+        {
+            if (password.Length < MinLength) return ValidationResult.InvalidLength;
+            return ValidationResult.Valid;
+        }
+
+        public static string GetError(ValidationResult result) =>
+            result switch
+            {
+                ValidationResult.InvalidLength => $"Password must be between {MinLength} and {MaxLength}",
+                _ => throw new ArgumentOutOfRangeException(nameof(result), result,
+                    $"No error text for this " + nameof(ValidationResult))
+            };
+
+        public enum ValidationResult
+        {
+            Valid,
+            InvalidLength
+        }
+    }
+}
---
+++ Assets/Project/Login/Username.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Project.Login
+{
+    public static class Username
+    {
+        public const int MinLength = 1;
+        public const int MaxLength = 32;
+
+        public static ValidationResult Validate(string username)
+        {
+            if (username.Length is < MinLength or > MaxLength) return ValidationResult.InvalidLength;
+            return ValidationResult.Valid;
+        }
+        
+        public static string GetError(ValidationResult result) =>
+            result switch
+            {
+                ValidationResult.InvalidLength => $"Username must be between {MinLength} and {MaxLength}",
+                _ => throw new ArgumentOutOfRangeException(nameof(result), result,
+                    $"No error text for this " + nameof(ValidationResult))
+            };
+
+        public enum ValidationResult
+        {
+            Valid,
+            InvalidLength
+        }
+    }
+}
