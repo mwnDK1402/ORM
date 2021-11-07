8bffa2fca491ed188278ad5330d4f04c81670a1b
Mathias Wagner Nielsen <mathiasvalentinwagner@gmail.com>
2021-11-07 15:49:57 +0100

    feat(hasher): add installer

---
+++ Assets/Project/Security/HasherInstaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Project.Security
+{
+    [CreateAssetMenu(menuName = "Installers/Create " + nameof(HasherInstaller), fileName = nameof(HasherInstaller))]
+    public sealed class HasherInstaller : ScriptableObjectInstaller<HasherInstaller>
+    {
+        [SerializeField] private int degreesOfParallelism = 1;
+        [SerializeField] private int iterations = 1;
+        [SerializeField] private int memorySize = 16;
+
+        public override void InstallBindings() => Container.Bind<Hasher>().AsSingle()
+            .WithArguments(degreesOfParallelism, iterations, memorySize);
+    }
+}
